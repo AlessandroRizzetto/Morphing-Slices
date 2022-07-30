c8a59989ad3b10047b1ccc45c5750c7b1dcf3d38
Thomas Andreatta <thomasandreatta2000@gmail.com>
2022-07-30 22:10:58 +0000

    check.sh

--- check.sh
+++ check.sh
@@ -1,39 +1,7 @@
 #!/usr/bin/bash
-if [[ $1 = "ring" ]]
-then
-	echo -e "\t\t\t\t\t===== RING ======"
-	for i in {1..10}
-	do
-		echo -e "\n\n\t\t===== S$i =====\n"
-		sudo ovs-ofctl dump-flows s$i
-	done
-elif [[ $1 = "tree" ]]
-then
-	echo -e "\t\t\t\t\t===== TREE ======"
-	for i in {1..10}
-	do
-		echo -e "\n\n\t\t===== S$i =====\n"
-		sudo ovs-ofctl dump-flows s$i
-	done
-elif [[ $1 = "linear" ]]
-then
-	echo -e "\t\t\t\t\t===== LINEAR ======"
-	for i in {1..10}
-	do
-		echo -e "\n\n\t\t===== S$i =====\n"
-		sudo ovs-ofctl dump-flows s$i
-	done
-elif [[ $1 = "star" ]]
-then
-	echo -e "\t\t\t\t\t===== STAR ======"
-	for i in {1..10}
-	do
-		echo -e "\n\n\t\t===== S$i =====\n"
-		sudo ovs-ofctl dump-flows s$i
-	done
-
-else
-	echo -e "bash check.sh [topology]"
-	echo -e "[ring,star,tree,linear]"
-fi
+for i in {1..10}
+do
+	echo -e "\n\n\t\t===== S$i =====\n"
+	sudo ovs-ofctl dump-flows s$i
+done
 
